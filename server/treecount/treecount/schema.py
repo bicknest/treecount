9f767a8de2a3606d4511421fa9ca7c916b9151ab
Nicholas Best <nick.best5456@gmail.com>
2019-09-20 21:53:44 -0700

    fix a couple mistakes

--- server/treecount/treecount/schema.py
+++ server/treecount/treecount/schema.py
@@ -3,7 +3,7 @@ import graphene
 import core.schema
 
 
-class Query(links.schema.Query, graphene.ObjectType):
+class Query(core.schema.Query, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
